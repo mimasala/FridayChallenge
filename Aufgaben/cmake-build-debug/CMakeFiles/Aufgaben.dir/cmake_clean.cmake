35541190c3715d6b26b07571fe94b5e7ee2e38a0
mimasala <65661176+mimasala@users.noreply.github.com>
2021-11-05 10:04:29 +0100

    more THINGS

--- Aufgaben/cmake-build-debug/CMakeFiles/Aufgaben.dir/cmake_clean.cmake
+++ Aufgaben/cmake-build-debug/CMakeFiles/Aufgaben.dir/cmake_clean.cmake
@@ -2,6 +2,7 @@ file(REMOVE_RECURSE
   "Aufgaben.exe"
   "Aufgaben.exe.manifest"
   "Aufgaben.pdb"
+  "CMakeFiles/Aufgaben.dir/BADAPPLE.c.obj"
   "CMakeFiles/Aufgaben.dir/main.c.obj"
   "CMakeFiles/Aufgaben.dir/notenrechner.c.obj"
   "CMakeFiles/Aufgaben.dir/utils.c.obj"
